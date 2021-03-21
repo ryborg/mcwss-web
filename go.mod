c1cdf4f2ac6752992183ff29372ff1dda3abe8c5
root <root@DESKTOP-B0C45D3.localdomain>
2021-03-21 18:10:55 -0400

    web

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module main
 go 1.16
 
 require (
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19 // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-bootstrap/go-bootstrap v0.0.0-20200531174834-c980814f3a58 // indirect
+	github.com/kardianos/osext v0.0.0-20190222173326-2bc1f35cddc0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/ryborg/mcwss v1.3.1-0.20210321210724-f9e16e06f459 // indirect
 	github.com/sandertv/mcwss v1.2.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,28 @@
-
 package main
 
 import (
 	"github.com/ryborg/mcwss"
+//	"io/ioutil"
+//	"log"
+	"net/http"
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+
 )
 
 func main() {
+router := gin.Default()
+router.Use(static.Serve("/", static.LocalFile("./views",true)))
+api := router.Group("/api")
+	{
+		api.GET("/", func(c *gin.Context){
+			c.JSON(http.StatusOK, gin.H {
+			"message": "pong",
+			})
+		})
+	}
+//router.Run(":8100")
+
 
 	server := mcwss.NewServer(nil)
 
@@ -14,5 +31,6 @@ player.Tell("Hello %s!", player.Name())
 })
 	server.OnDisconnection(func(player *mcwss.Player){
 	})
-	server.Run()
+	go server.Run()
+	router.Run(":8100")
 }
